6914e2719be11c1d50e227d28d88ebd36ae467b5
jimmykiedis <jinkiedis@icloud.com>
2025-10-24 21:56:30 -0400

    Version 0.0.4

--- mpi.py
+++ mpi.py
@@ -7,20 +7,20 @@ size = comm.Get_size()
 n = 10                                  #nosso array vai começar com 10
 
 if rank == 0:
-    data = np.random.ramdom(n)          #vamos criar um array de n doubles aleatórios
+    data = np.random.random(n)          #vamos criar um array de n doubles aleatórios
 
     inicio = MPI.Wtime()
-    comm.send(data, dest=1)             #enviando para o processo 1
-    comm.recv(data, source=1)           #recebendo os dados de volta do processo 1
-    fim = MPI.Wtime
+    comm.Send(data, dest=1)             #enviando para o processo 1
+    comm.Recv(data, source=1)           #recebendo os dados de volta do processo 1
+    fim = MPI.Wtime()
 
     tempo = fim - inicio
     print(f"[P0] tamanho {n} doubles | tempo total: {tempo:.6f} s")
 
 elif rank == 1:
-    message = np.empity(n, dtype='d')   #vamos criar um espaço vazio para guardados os dados recebidos
-    comm.recv(message, source=0)         #agora vamos receber os dados
-    comm.send(message, dest=0)           #vamos retornar ele ao p0
+    message = np.empty(n, dtype='d')   #vamos criar um espaço vazio para guardados os dados recebidos
+    comm.Recv(message, source=0)         #agora vamos receber os dados
+    comm.Send(message, dest=0)           #vamos retornar ele ao p0
     print("processo 1 devolveu os dados!")
 
 
